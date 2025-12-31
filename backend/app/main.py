b290e22db616830de9e81575326d0fae57ceeb8f
Ashutosh1921 <as7857139@gmail.com>
2025-12-31 13:39:51 +0530

    chore: setup production server and fix static directory error

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
@@ -19,7 +18,6 @@ if settings.BACKEND_CORS_ORIGINS:
     )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 @app.get("/")
 def read_root():
